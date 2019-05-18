1bc455d872d84c045ff9ec70629eb6aef2f26294
john andrey <andrey_201015@hotmail.com>
2019-05-17 21:07:16 -0500

    Add package cowsay

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const cowsay = require("cowsay");
+
+console.log(
+  cowsay.say({
+    text: "hola soy cowsay",
+    e: "oO",
+    T: "U"
+  })
+)
