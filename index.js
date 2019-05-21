37d4c0dd86275de559481912fb948fccddd4e796
John Andrey <andrey_201015@hotmail.com>
2019-05-20 21:54:54 -0500

    Add example

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require('./src/call');
+
+// call.greet('John Andrey')
+
+// call.withPromise('John', 'Serrano')
+// .then(n => console.log(n))
+
+async function CallWithPromise() {
+  try {
+    const result = await call.withPromise('John', 'Serrano')
+    console.log(result)
+  } catch (error) {
+    console.error(`Algo salio mal -> ${error}`)
+  }
+}
+
+CallWithPromise()
