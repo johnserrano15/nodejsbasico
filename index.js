85bee80bf2ff124ab4be3e4b6915f2fb41e0365f
John Andrey <andrey_201015@hotmail.com>
2019-05-22 21:44:13 -0500

    Transformando functions a promises

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const fs = require('fs')
+const ops = require('./src/fileops')
+
+const readFile = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, list) => {
+      if (err) reject(err)
+
+      resolve(list.split('\n'))
+    })
+  })
+}
+
+const writeFile = (path, value) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, value.join('\n'), (err, list) => {
+      if (err) reject(err)
+
+      resolve(true)
+    })
+  })
+}
+
+async function main () {
+  try {
+    const numbers = await readFile('./resources/number.txt')
+    const newNumbers = ops.incrementValues(numbers)
+
+    const names = await readFile('./resources/name.txt')
+    const newNames = ops.callNames(names)
+
+    const saveNumbers = await writeFile('./resources/numberNew.txt', newNumbers)
+    const saveNames = await writeFile('./resources/nameNew.txt', newNames)
+    
+    console.log(saveNumbers)
+    console.log(saveNames)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+main()
+
+
