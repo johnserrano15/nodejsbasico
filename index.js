89be553644c3e29e13bedef40ed4b8d4e8065e55
John Andrey <andrey_201015@hotmail.com>
2019-05-23 22:09:34 -0500

    Add child process

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const cp = require('child_process')
+
+async function excmm(command){
+  await cp.exec(command, (err, out, sterr) => {
+    if(err) throw err;
+    if(out) console.log(`Out: ${out}`);
+    if(sterr) console.log(`sterr: ${sterr}`);
+  })
+}
+
+excmm("node ./src/script --base=5 --f=1e9")
