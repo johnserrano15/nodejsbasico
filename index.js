e1e461f5d85caa1de85ec395cad84bfc835e0a77
John Andrey <andrey_201015@hotmail.com>
2019-05-27 21:59:54 -0500

    Changes create server

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const http = require('http')
+const fs = require('fs')
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  fs.readFile('./resources/my_page.html', (err, content) => {
+    if (err) { console.error(err); return; }
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(content, 'utf-8');
+  })
+})
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+// Otra forma de crear un server basic
+/* http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.write('Hello World!')
+  res.end()
+}).listen(8080) */
