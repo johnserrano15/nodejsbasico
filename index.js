5f8191197488e5d629e81cbd112f787222ed69f6
John Andrey <andrey_201015@hotmail.com>
2019-05-28 21:49:06 -0500

    Changes express api

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const { PORT } = require("./config");
 
-require("./routes/views")(app);
 require("./routes/api")(app);
+require("./routes/views")(app);
 
 function init(){
     console.log("Iniciando instancia de Express...");
-    app.listen(PORT, ()=>{
+    app.listen(PORT, () => {
         console.log("El servidor Express esta activo.");
     });
 }
