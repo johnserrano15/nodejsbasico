a92da76be1170d3b8225986bde73d02d8ca86e5d
John Andrey <andrey_201015@hotmail.com>
2019-05-30 22:01:36 -0500

    Add socket.io

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-/**
- * 
- * ***************
- *  Comience aqui
- * ***************
- * 
- */
+const server = require('http').Server();
+// const server = require('http').createServer(); // Otra forma de hacerlo
+// https://nodejs.org/en/docs/guides/anatomy-of-an-http-transaction/#create-the-server
+const io = require('socket.io')(server);
+const port = require('./config').SERVER_PORT;
+
+const banner = `
+***********************
+  Basic Node.js Course
+    Course Project
+  Tic Tac Toe Server
+***********************
+Status: Online
+Listening: on port: ${port}
+`;
+
+io.on('connection', (socket) => {
+  socket.on('register', user => {
+    console.info(`User registed: ${user.name}`)
+  })
+})
+
+server.listen(port, () => {
+  console.info(banner);
+});
